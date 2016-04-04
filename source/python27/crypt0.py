cf0de8f41e29f2b7db1aac319563151011def201
Zachary King <kingzach77@gmail.com>
2016-04-04 08:30:32 -0500

    Crypt0 and Crypt1 done

--- source/python27/crypt0.py
+++ source/python27/crypt0.py
@@ -2,11 +2,12 @@ from Crypto.Cipher import AES
 
 # Encryption
 encryption_suite = AES.new('key of length 16', AES.MODE_CBC, 'iv of length 16.')
-ciphertext = encryption_suite.encrypt("Super secret message: pigs fly??")
+ciphertext = encryption_suite.encrypt("Super secret message. top secret")
 
 # Decryption
 decryption_suite = AES.new('key of length 16', AES.MODE_CBC, 'iv of length 16.')
 plaintext = decryption_suite.decrypt(ciphertext)
 
+print('Original: Super secret message. top secret')
 print('Ciphertext: %s' % ciphertext)
-print('Plaintext: %s' % plaintext)
+print('Decrypted: %s' % plaintext)
--- source/python27/crypt1.py
+++ source/python27/crypt1.py
@@ -4,12 +4,11 @@ import hashlib
 
 def get_hash(msg):
     # Create hash, using msg converted to bytes
-    return hashlib.md5(bytes(msg, 'utf-8')).hexdigest()
+    return hashlib.md5(msg).hexdigest()
 
 def pad(s):
-    s = bytes(s, 'utf-8')
     s = s + b'\0' * (AES.block_size - len(s) % AES.block_size)
-    return s.decode('utf-8')
+    return s
 
 def encrypt(message, key):
     message = pad(message)
